c18be3c329878f2692afc17375e439fa0f61d107
skzidev <chervonijpes@gmail.com>
2026-01-06 16:52:56 -0500

    Fix failing tests
    Fixed the tests failing in CI

--- tests/unit/test_argsparser.c
+++ tests/unit/test_argsparser.c
@@ -8,17 +8,25 @@ void test_argsparser(char* argv[], int argc, CompilerOptions expectedOpts){
 	// Add your test logic here
 	CompilerOptions recievedOpts = ParseArgs(argc, argv);
 
+	printf(recievedOpts.inputFiles == NULL ? "Found no input files\n" : "Found input files\n");
+
+	printf("asserting %b == %b\n", recievedOpts.silent, expectedOpts.silent);
 	assert(recievedOpts.silent == expectedOpts.silent);
+	printf("asserting %b == %b\n", recievedOpts.dumpAST, expectedOpts.dumpAST);
 	assert(recievedOpts.dumpAST == expectedOpts.dumpAST);
+	printf("asserting %b == %b\n", recievedOpts.help, expectedOpts.help);
 	assert(recievedOpts.help == expectedOpts.help);
+	printf("asserting %b == %b\n", recievedOpts.version, expectedOpts.version);
 	assert(recievedOpts.version == expectedOpts.version);
-	assert(recievedOpts.silent == expectedOpts.silent);
+	printf("asserting %b == %b\n", recievedOpts.verbose, expectedOpts.verbose);
+	assert(recievedOpts.verbose == expectedOpts.verbose);
+
 	// TODO ensure that input files list is the same
 }
 
 int main(){
-    char* set1[] = { "-b", "-s", "./path/to/file.vty" };
-    const int len1 = 2;
+    char* set1[] = { "./bin/verity", "-b", "-s", "./path/to/file.vty" };
+    const int len1 = 4;
     CompilerOptions expected1 = {
         .version=false,
         .dumpAST=false,
